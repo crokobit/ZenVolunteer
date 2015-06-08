4ff31217b92cbac93134296543f5614554e745fc
crokobit <crokobit@gmail.com>
2015-06-08 23:38:35 +0800

    ZenPeriodsController #show #new #create

--- app/controllers/zen_periods_controller.rb
+++ app/controllers/zen_periods_controller.rb
@@ -4,15 +4,20 @@ class ZenPeriodsController < ApplicationController
   end
   
   def show
-    
+    @zen_period = ZenPeriod.find(params[:id])
   end
 
   def new
-    
+    @form = ZenPeriodForm.new(ZenPeriod.new)
   end
 
   def create
-    
+    @form = ZenPeriodForm.new(ZenPeriod.new)
+    if @form.validate(params[:zen_period])
+      @form.save
+      redirect_to zen_periods_path
+    else
+    end
   end
 
   def edit
---
+++ app/forms/zen_period_form.rb
@@ -0,0 +1,6 @@
+class ZenPeriodForm < Reform::Form
+  property :start_datetime
+  property :end_datetime
+  property :zen_type
+  property :name
+end
