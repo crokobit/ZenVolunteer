c00f9a87730772f384affe8b0c604798ff7dfc72
crokobit <crokobit@gmail.com>
2015-06-08 21:33:49 +0800

    add generator settings for not generating no use files

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module ZenVolunteer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |generate|
+      generate.helper false
+      generate.decorator false 
+      generate.assets false                           
+      generate.view_specs false 
+      generate.helper_specs false 
+      generate.controller_specs false
+      generate.test_framework false
+    end 
   end
 end
