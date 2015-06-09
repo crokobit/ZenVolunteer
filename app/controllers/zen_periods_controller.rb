65578a41e5a9b8ff3dff29ec323cf2f6735dce88
Ivan Ho <ivan.ho@jcmanagement.com.tw>
2015-06-09 16:39:16 +0800

    complete ZenPeriod CRUD and extract form view

--- app/controllers/zen_periods_controller.rb
+++ app/controllers/zen_periods_controller.rb
@@ -17,18 +17,27 @@ class ZenPeriodsController < ApplicationController
       @form.save
       redirect_to zen_periods_path
     else
+      render :new
     end
   end
 
   def edit
-    
+    @form = ZenPeriodForm.new(ZenPeriod.find(params[:id]))
   end
 
   def update
-    
+    @form = ZenPeriodForm.new(ZenPeriod.find(params[:id]))
+    if @form.validate(params[:zen_period])
+      @form.save
+      redirect_to zen_periods_path
+    else
+      render :edit
+    end
   end
   
   def destroy
-    
+    @zen_period = ZenPeriod.find(params[:id])
+    @zen_period.destroy
+    redirect_to zen_periods_path
   end
 end
