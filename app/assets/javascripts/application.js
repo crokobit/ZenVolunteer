e5dccf1f0195b73dd059f67b3ac8bdf91b787b58
crokobit <crokobit@gmail.com>
2015-06-11 23:36:56 +0800

    add datepicker js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,9 @@
 
 $(document).ready(function() {
   $(".basic-single").select2();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 });
+
