8799746e113dfec17100354b079e3a85f0931ebc
Ivan Ho <ivan.ho@jcmanagement.com.tw>
2015-06-12 10:19:18 +0800

    fix js problem (calused by Turbolinks)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,17 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 //= require materialize-sprockets
 //= require select2
-
-
-$(document).ready(function() {
-  $(".basic-single").select2();
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
-});
-
+//= require_tree .
