f07f9e5e23839de1e737b18e7f1b19f2fa5148a5
Ivan <crokobit@gmail.com>
2015-09-19 20:07:13 +0800

    fix bug

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -15,6 +15,18 @@ class SurveysController < ApplicationController
     redirect_to zen_periods_url(volunteer_id: @volunteer.id)
   end
 
+  # TODO
+  def update
+    @volunteer = Volunteer.find(params[:volunteer_id])
+    @survey = @volunteer.survey || Survey.new
+
+    @survey.update(survey_param)
+    @survey.volunteer = @volunteer
+    @survey.save
+
+    redirect_to zen_periods_url(volunteer_id: @volunteer.id)
+  end
+
   private
   def survey_param
     params.require(:survey).permit!
